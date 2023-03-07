b0b143ccaa4b792d9dd935e373ce250309c44c6f
danutz <iovanadaniel3@gmail.com>
2023-03-08 00:18:16 +0200

    make static String

--- IovanaFlorinDaniel/src/ro/ase/acs/classes/DatabaseConnector.java
+++ IovanaFlorinDaniel/src/ro/ase/acs/classes/DatabaseConnector.java
@@ -7,7 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnector implements DatabaseConnection {
-    private final String DB_URL = "jdbc:sqlite:database.db";
+    private static final String DB_URL = "jdbc:sqlite:database.db";
 
     @Override
     public Connection getConnection() throws SQLException {
